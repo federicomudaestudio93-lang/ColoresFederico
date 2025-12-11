5d6c571dd31662b364cc46b112aff165dce96ced
Juan <b1@Tr71-W10.cen.local>
2025-12-11 20:34:27 -0300

    "Primera Version"

---
+++ src/coloresfederico/ColoresFederico.java
@@ -0,0 +1,16 @@
+package coloresfederico;
+
+public class ColoresFederico {
+
+    public static void main(String[] args) {
+        String color1, color2, color3;
+        color1 = "AZUL";
+        color2 = "ROJO";
+        color3 = "VERDE";
+        System.out.printf("%s es mi primer color favorito\n", color1);
+        System.out.printf("%s es mi segundo color favorito\n", color2);
+        System.out.printf("%s es mi tercer color favorito\n", color3);
+
+    }
+
+}
