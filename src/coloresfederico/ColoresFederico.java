bec302d40c0466e91c203debf746dac51465ffd0
b2 <b2@Ter59-W10.cen.local>
2025-12-11 21:14:11 -0300

    "cambio de colores favoritos"

--- src/coloresfederico/ColoresFederico.java
+++ src/coloresfederico/ColoresFederico.java
@@ -4,9 +4,9 @@ public class ColoresFederico {
 
     public static void main(String[] args) {
         String color1, color2, color3;
-        color1 = "AZUL";
+        color1 = "VIOLETA";
         color2 = "ROJO";
-        color3 = "VERDE";
+        color3 = "AZUL";
         System.out.printf("%s es mi primer color favorito\n", color1);
         System.out.printf("%s es mi segundo color favorito\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
